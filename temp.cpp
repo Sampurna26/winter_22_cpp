eccfe62318a65a8de16bdc59cc912bafcdf2ff30
Sampurna26 <91620842+Sampurna26@users.noreply.github.com>
2022-12-16 17:17:35 +0530

    Add files via upload

---
+++ temp.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    double c,f;
+    cout<<"enter the value of f : ";
+    cin>>f;
+    c=(f-32)/9*5;
+    cout<<"the value of c:"<<c;
+    return 0;    
+}
+
