97e506dddd87e3304882b9a05654453f70f4e7f7
Sampurna26 <91620842+Sampurna26@users.noreply.github.com>
2022-12-17 02:18:08 +0530

    Add files via upload

---
+++ pattern2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a,i,n=5;
+     for (a=1;a<=1;a++){
+        cout<<a;
+}
+cout<<endl;
+
+     for (a=1;a<=2;a++){
+        cout<<a;
+}
+cout<<endl;
+
+     for (a=1;a<=3;a++){
+        cout<<a;
+}
+cout<<endl;
+
+     for (a=1;a<=4;a++){
+        cout<<a;
+}
+cout<<endl;
+
+    
+
+     for (i=1;i<=n;i++){
+        cout<<i
+        ;
+}
+cout<<endl;
+return 0;
+}
+
