ae7cd6499ef3e5fd1c155a5d03cbdbd54ff3052d
Sampurna26 <91620842+Sampurna26@users.noreply.github.com>
2022-12-19 17:39:38 +0530

    Update pattern2.cpp

--- pattern2.cpp
+++ pattern2.cpp
@@ -1,34 +1,24 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 int main(){
-    int a,i,n=5;
-     for (a=1;a<=1;a++){
-        cout<<a;
-}
-cout<<endl;
-
-     for (a=1;a<=2;a++){
-        cout<<a;
-}
-cout<<endl;
-
-     for (a=1;a<=3;a++){
-        cout<<a;
-}
-cout<<endl;
-
-     for (a=1;a<=4;a++){
-        cout<<a;
-}
-cout<<endl;
-
+    int i,a,j=5,n=5;
+    for (a=1;a<=j;a++){
+    for(i=1;i<=a;i++){
+        cout<<i;
+    }
+    cout<<endl;
+    }
     
-
-     for (i=1;i<=n;i++){
-        cout<<i
-        ;
-}
-cout<<endl;
-return 0;
+    return 0;
 }
 
+
+/*
+output:
+
+1
+12
+123
+1234
+12345
+*/
