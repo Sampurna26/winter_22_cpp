a262bf7fb4ffa8fd3f6434b5856afd5b5ea2d5b0
Sampurna26 <91620842+Sampurna26@users.noreply.github.com>
2022-12-21 12:07:31 +0530

    Add files via upload

---
+++ factorial.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,n,p=1;
+    cout<<"enter the value of n";
+    cin>>n;
+    for(i=1;i<=n;i++){
+        p=p*i;
+        
+
+    }
+    cout<<p;
+    return 0;
+}
