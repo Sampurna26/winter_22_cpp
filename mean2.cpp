ab2a986bd7df20069cf5fc0cdc38b7cf865a789d
Sampurna26 <91620842+Sampurna26@users.noreply.github.com>
2022-12-22 01:51:17 +0530

    Add files via upload

---
+++ mean2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,n;
+    double sum,mean;
+    cout<<"enter the value of n :";
+    cin>>n;
+    i=(n+1)/2;
+    cout<<i;
+    return 0;
+    
+
+}
