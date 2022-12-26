ce01732f2dbb336acea864c282757cbc76b203b5
Sampurna26 <91620842+Sampurna26@users.noreply.github.com>
2022-12-26 16:45:32 +0530

    Update swap1.cpp

--- swap1.cpp
+++ swap1.cpp
@@ -1,28 +1,40 @@
+
+
 #include<iostream>
 using namespace std;
 int main(){
-    int i,max,min,n,j ;
-    int array[n] ;
-    cout<<"enter the size of n";
+    int i,max,min,n,j,position ;
+    cout<<"enter the size of n=";
     cin>>n;
-    cout<<endl<<n<<"enter the elements in n";
+    int array[n];
+    int max_position, min_position;
+    cout<<endl<<"enter the elements in n=";
     for(i=0;i<n;i++)
     cin>>array[i];
     max=array[0];
     min=array[0];
     for(i=1;i<n;i++){
-        if(array[i]>max)
-        max=array[i];
-        if(array[i]<min)
-        min=array[i];
+        if(array[i]>max){
+            max=array[i];
+            max_position=i;
+
+        }
+        if(array[i]<min){
+            min=array[i];
+            min_position=i;
+        }
     }
+    cout<<"the max number of array="<<max<<endl;
+    cout<<"the min number of array="<<min;
+    j=array[max_position];
+    array[max_position]=array[min_position];
+    array[min_position]=j;
     cout<<endl;
-    cout<<"max="<<max<<"min="<<min<<endl;
-    j=max;
-    max=min;
-    min=j;
-    cout<<endl;
-    cout<<"max="<<max<<"min="<<min<<endl;
+    
+    cout<<"the new swapped array :"<<endl;
+    for(i=0;i<n;i++){
+        cout<<array[i]<<" ";
+    }
     return 0;
     
 
