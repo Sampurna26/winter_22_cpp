b4355507acd5c7595e7afdab50e8affd5b53d304
Sampurna26 <91620842+Sampurna26@users.noreply.github.com>
2022-12-27 20:18:07 +0530

    Add files via upload

---
+++ sentence.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string str;
+    int i,count=0;
+    cout<<"enter the elements in the string="<<str;
+    getline(cin,str);
+    int n=str.size();
+    for(i=0;i<n;i++){
+        if(str[i]==' '){
+            count++;
+            cout<<endl;
+        }
+        else{
+            cout<<str[i];
+        }
+        
+    }
+    cout<<endl;
+    cout<<"enter the no. of words="<<count+1;
+
+
+    return 0;
+}
