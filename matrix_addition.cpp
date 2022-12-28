5187943efaccedc5f39ae1760a91d867950209fe
Sampurna26 <91620842+Sampurna26@users.noreply.github.com>
2022-12-28 18:50:37 +0530

    Add files via upload

---
+++ matrix_addition.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,j,m,n;
+    cout<<"enter the value of row and column";
+    cin>>m>>n;
+    int a[m][n];
+    int b[m][n];
+    int c[m][n];
+    //data entered in the matrix a
+    cout<<"enter the matrix a"<<endl;
+    for(i=0;i<m;i++){
+        for(j=0;j<n;j++){
+            cin>>a[i][j];
+        }
+    }
+    cout<<"enter the matrix b"<<endl;
+    for(i=0;i<m;i++){
+        for(j=0;j<n;j++){
+            cin>>b[i][j];
+        }
+    }
+    for(i=0;i<m;i++){
+        for(j=0;j<n;j++){
+            c[i][j]=a[i][j]+b[i][j];
+            
+        }
+    }
+    cout<<"adding the matrix a and b"<<endl;
+    for(i=0;i<m;i++){
+        for(j=0;j<n;j++){
+            cout<<c[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+    
+    
+    
+}
