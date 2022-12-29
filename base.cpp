62577a4c4c8ca888ebb89ec78f156dfa53f4f37c
Sampurna26 <91620842+Sampurna26@users.noreply.github.com>
2022-12-29 18:58:35 +0530

    Add files via upload

---
+++ base.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+class Base{
+    public:
+      virtual void show(){
+        cout<<"Base class";
+    }
+};
+class Derv: public Base{
+    public:
+    void show(){
+        cout<<"inside derived";
+    }
+};
+int main(){
+    Derv d;
+    Base*ptr;
+    ptr= &d;
+    ptr-> show();
+    return 0;
+}
